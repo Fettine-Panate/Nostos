57c7dfd5a82a6b0a4a50278c8165a988bc575516
Stusio <france.destasio@studenti.unina.it>
2023-05-19 17:57:43 +0200

    Initial Commit

---
+++ MacroChallengeCode/MacroChallengeCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacroChallengeCodeApp.swift
+//  MacroChallengeCode
+//
+//  Created by Francesco De Stasio on 19/05/23.
+//
+
+import SwiftUI
+
+@main
+struct MacroChallengeCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
