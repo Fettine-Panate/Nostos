a97e1d03a655a824483647248bf5219ba7494c12
Martons00 <raffaelemartone34@gmail.com>
2023-05-26 14:39:44 +0200

    Detail MapView

--- MacroChallengeCode/Views/MapView.swift
+++ MacroChallengeCode/Views/MapView.swift
@@ -9,14 +9,15 @@ import SwiftUI
 import CoreLocation
 
 let degreesOnMeter = 0.0000089
+let magnitudeinm = 250.0
 
 struct MapView: View {
     var path : PathCustom
-    
-    @ObservedObject var compassHeading = CompassHeading()
     @ObservedObject var locationManager = LocationManager.shared
     
     var currentUserLocation : CLLocation
+    @GestureState private var magnification: CGFloat = 1.0
+    @State private var currentValue: CGFloat = 0.0
     @State var magnitude = 250.0
     
     
@@ -24,6 +25,11 @@ struct MapView: View {
     var body: some View {
         GeometryReader { geometry in
             ZStack {
+                Color.blue
+                
+                        Text("magnitude: \(magnitude) m ")
+                            .font(.largeTitle)
+                            .padding(.top,500)
                 ForEach(path.getLocations(), id: \.self ){ loc in
                     if isDisplayable(loc: loc, currentLocation: currentUserLocation, sizeOfScreen: geometry.size, longitudeMetersMax: magnitude){
                         let position = calculatePosition2(loc: loc, currentLocation: currentUserLocation, sizeOfScreen: geometry.size, longitudeMetersMax: magnitude)
@@ -33,9 +39,17 @@ struct MapView: View {
                     }
                 }
             }
+                .gesture(
+                    MagnificationGesture()
+                        .updating($magnification) { value, magnification, _ in
+                            magnification = value
+                        }
+                        .onChanged { value in
+                            currentValue = value
+                            magnitude = value * magnitudeinm
+                        }
+                )
         }
-        .rotationEffect(Angle(degrees: self.compassHeading.degrees))
-        //Flickkera un po
     }
 }
 
