16fe371c3aad9e44bc29bf57d918ca28d557425d
Pietro Ciuci <pietro.ciuci@gmail.com>
2023-06-06 18:50:32 +0200

    Updated Widget View
    Updated Live Activity and Dynamic Island design. Stop logic is still an issue.

--- SunsetWidget/SunsetWidgetLiveActivity.swift
+++ SunsetWidget/SunsetWidgetLiveActivity.swift
@@ -22,94 +22,99 @@ struct SunsetWidgetAttributes: ActivityAttributes {
         return start...end
     }
 }
-    
-    struct SunsetWidgetLiveActivity: Widget {
-        var body: some WidgetConfiguration {
-            ActivityConfiguration(for: SunsetWidgetAttributes.self) { context in
-                // Lock screen/banner UI goes here
-                ZStack(alignment: .leading) {
-                    RoundedRectangle(cornerRadius: 15, style: .continuous)
-                        .fill(Color.red)
-                    VStack(alignment: .leading, spacing: 5) {
-                        Text("Sunset")
-                            .font(.system(size: 24, design: .rounded))
-                            .fontWeight(.medium)
-                            .foregroundColor(.white)
-                        ProgressView(timerInterval: context.attributes.progressInterval, countsDown: true) {
-                            Text("Time left")
-                                .font(.caption)
-                        }
-                        .tint(.white)
-                        .foregroundColor(.white)
-                        Text(context.attributes.sunsetTime, style: .time)
-                            .font(.system(size: 20, design: .rounded))
-                            .fontWeight(.medium)
-                            .foregroundColor(.white)
-                    }
-                    .padding(.horizontal)
-                    .padding(.vertical, 10)
-                    //                .padding(15)
-                }
-                
-            } dynamicIsland: { context in
-                DynamicIsland {
-                    // Expanded UI goes here.  Compose the expanded UI through
-                    // various regions, like leading/trailing/center/bottom
-                    DynamicIslandExpandedRegion(.leading) {
-                        // Something I guess...?
-                    }
-                    DynamicIslandExpandedRegion(.trailing) {
-                        // Something I guess...?
-                    }
-                    DynamicIslandExpandedRegion(.bottom) {
-                        ProgressView(timerInterval: context.attributes.progressInterval, countsDown: true) {
-                            HStack {
-                                Image(systemName: "sunset.fill")
-                                    .foregroundColor(Color.red)
-                                Spacer()
-                                Text(context.attributes.sunsetTime, style: .time)
-                                    .font(.system(size: 16, design: .rounded))
-                                    .fontWeight(.medium)
-                                    .foregroundColor(.white)
-                            }
-                        }
-                        .tint(.white)
-                        .foregroundColor(.white)
-                        .padding(.horizontal)
-                    }
-                } compactLeading: {
+
+struct SunsetWidgetLiveActivity: Widget {
+    var body: some WidgetConfiguration {
+        ActivityConfiguration(for: SunsetWidgetAttributes.self) { context in
+            // Lock screen/banner UI goes here
+            ZStack(alignment: .leading) {
+                RoundedRectangle(cornerRadius: 15, style: .continuous)
+                    .fill(Color.black)
+                HStack(spacing: 10) {
+                    //Text("Sunset")
                     Image(systemName: "sunset.fill")
-                        .foregroundColor(Color.red)
-                } compactTrailing: {
+                        .font(.system(size: 35))
+                        .foregroundColor(.white)
+                        .padding(.vertical, 15)
+                    ProgressView(timerInterval: context.attributes.progressInterval, countsDown: true) {
+                        Text("Time left")
+                            .font(.caption)
+                    }
+                    .tint(.white)
+                    .foregroundColor(.white)
+                    .padding(.vertical, -15)
                     Text(context.attributes.sunsetTime, style: .time)
-                        .font(.system(.body, design: .rounded))
+                        .font(.system(size: 20, design: .rounded))
                         .fontWeight(.medium)
                         .foregroundColor(.white)
-                } minimal: {
-                    Text("Min")
                 }
-                .widgetURL(URL(string: "http://www.apple.com"))
-                .keylineTint(Color.red)
+                .padding(.horizontal)
+                .padding(.vertical, 10)
             }
+        } dynamicIsland: { context in
+            DynamicIsland {
+                // Expanded UI goes here.  Compose the expanded UI through
+                // various regions, like leading/trailing/center/bottom
+                DynamicIslandExpandedRegion(.leading) {
+                    Image(systemName: "sunset.fill")
+                        .foregroundColor(Color.white)
+                        .font(.title)
+                }
+                DynamicIslandExpandedRegion(.trailing) {
+                    Text(context.attributes.sunsetTime, style: .time)
+                        .font(.system(size: 18, design: .rounded))
+                        .fontWeight(.medium)
+                        .foregroundColor(.gray)
+                        .padding(.vertical, 10)
+                }
+                DynamicIslandExpandedRegion(.center) {
+                    // Nothing here
+                }
+                DynamicIslandExpandedRegion(.bottom) {
+                    ProgressView(timerInterval: context.attributes.progressInterval, countsDown: true) {
+                        // Label needed?
+                    }
+                    .tint(.white)
+                    .foregroundColor(.white)
+                    .scaleEffect(x: 1.5, y: 1.5, anchor: .center)
+                    .padding(.horizontal, 57)
+                    .padding(.vertical, 5)
+                }
+            } compactLeading: {
+                Image(systemName: "sunset.fill")
+                    .foregroundColor(Color.white)
+            } compactTrailing: {
+                Text(context.attributes.sunsetTime, style: .time)
+                    .font(.system(.body, design: .rounded))
+                    .fontWeight(.medium)
+                    .foregroundColor(.white)
+            } minimal: {
+                Image(systemName: "sunset.fill")
+                    .foregroundColor(Color.white)
+                    .padding()
+            }
+            .widgetURL(URL(string: "http://www.apple.com"))
+            .keylineTint(Color.white)
         }
     }
+}
+
+struct SunsetWidgetLiveActivity_Previews: PreviewProvider {
+    static let attributes = SunsetWidgetAttributes(sunsetTime: .now)
+    static let contentState = SunsetWidgetAttributes.ContentState()
     
-    struct SunsetWidgetLiveActivity_Previews: PreviewProvider {
-        static let attributes = SunsetWidgetAttributes(sunsetTime: .now)
-        static let contentState = SunsetWidgetAttributes.ContentState()
-        
-        static var previews: some View {
-            attributes
-                .previewContext(contentState, viewKind: .dynamicIsland(.compact))
-                .previewDisplayName("Island Compact")
-            attributes
-                .previewContext(contentState, viewKind: .dynamicIsland(.expanded))
-                .previewDisplayName("Island Expanded")
-            attributes
-                .previewContext(contentState, viewKind: .dynamicIsland(.minimal))
-                .previewDisplayName("Minimal")
-            attributes
-                .previewContext(contentState, viewKind: .content)
-                .previewDisplayName("Notification")
-        }
+    static var previews: some View {
+        attributes
+            .previewContext(contentState, viewKind: .dynamicIsland(.compact))
+            .previewDisplayName("Island Compact")
+        attributes
+            .previewContext(contentState, viewKind: .dynamicIsland(.expanded))
+            .previewDisplayName("Island Expanded")
+        attributes
+            .previewContext(contentState, viewKind: .dynamicIsland(.minimal))
+            .previewDisplayName("Minimal")
+        attributes
+            .previewContext(contentState, viewKind: .content)
+            .previewDisplayName("Notification")
     }
+}
