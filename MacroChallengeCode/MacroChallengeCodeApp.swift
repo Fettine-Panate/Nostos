6ec4111374f6ba788c436f3f9b21143acd20bbad
Pietro Ciuci <pietro.ciuci@gmail.com>
2023-06-07 17:56:00 +0200

    Update MacroChallengeCodeApp.swift

--- MacroChallengeCode/MacroChallengeCodeApp.swift
+++ MacroChallengeCode/MacroChallengeCodeApp.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 @main
 struct MacroChallengeCodeApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) private var appDelegate
     var body: some Scene {
         WindowGroup {
             ContentView()
