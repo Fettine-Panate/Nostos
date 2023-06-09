96d880f2373f5a8e9daa9d29023206f8cf98eb37
Pietro Ciuci <pietro.ciuci@gmail.com>
2023-06-09 19:47:41 +0200

    Update NotificationManagerDelegate.swift
    Scheduling logic for new reminder is still in progress

--- MacroChallengeCode/Utilities/NotificationManagerDelegate.swift
+++ MacroChallengeCode/Utilities/NotificationManagerDelegate.swift
@@ -8,8 +8,16 @@
 import Foundation
 import UserNotifications
 
+
+enum NotificationActions: String {
+    case remind
+}
+
 class NotificationManager: NSObject, ObservableObject, UNUserNotificationCenterDelegate {
+    
     static let shared = NotificationManager()
+    let center = UNUserNotificationCenter.current()
+    let content = UNMutableNotificationContent()
     
     private override init() {
         super.init()
@@ -30,24 +38,60 @@ class NotificationManager: NSObject, ObservableObject, UNUserNotificationCenterD
         }
     }
     
-    func sendNotification(with title: String, with body: String) {
+    func createNotification(title: String, body: String) {
         
-        UNUserNotificationCenter.current().getNotificationSettings { settings in
+        center.getNotificationSettings { settings in
             guard (settings.authorizationStatus == .authorized) ||
                     (settings.authorizationStatus == .provisional) else { return }
         }
-    
-        let content = UNMutableNotificationContent()
+        
         content.title = title
         content.body = body
         content.sound = .default
+        content.categoryIdentifier = "SUNSET_REMINDER"
         
-        let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: nil)
-        UNUserNotificationCenter.current().add(request)
+        let remindAction = UNNotificationAction(identifier: "REMIND_ACTION", title: "Remind me in 10 minutes", options: [])
+
+        let category =
+              UNNotificationCategory(identifier: "SUNSET_REMINDER",
+              actions: [remindAction],
+              intentIdentifiers: [],
+              options: []
+              )
+        
+        center.setNotificationCategories([category])
+        
+        scheduleNotification(timeInterval: 5.0)
+        
+    }
+    
+    func scheduleNotification(timeInterval: TimeInterval) {
+        
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: timeInterval, repeats: false)
+        let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+        
+        center.add(request) { error in
+            if let error = error {
+                print(error.localizedDescription)
+            }
+        }
     }
     
     func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
         completionHandler([ .sound, .badge])
+    }
+    
+    // Handle selected actions of notification
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        switch response.actionIdentifier {
+        case "REMIND_ACTION":
+            scheduleNotification(timeInterval: 3) //600
+            break
+        default:
+            print("Dunno mate")
+            break
+        }
+        completionHandler()
     }
     
 }
