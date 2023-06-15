cc5b0b53717db5203c43aad3c2845ffebdb3873f
Pietro Ciuci <pietro.ciuci@gmail.com>
2023-06-15 17:48:35 +0200

    Created AppDelegate
    This was done in order to prevent the app to send push notification to the user whenever he kills it

---
+++ MacroChallengeCode/Utilities/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  MacroChallengeCode
+//
+//  Created by Pietro Ciuci on 15/06/23.
+//
+
+import Foundation
+import SwiftUI
+
+class AppDelegate: UIResponder, UIApplicationDelegate {
+        
+    func applicationWillTerminate(_ application: UIApplication) {
+        // Prevent app to send you push notification if is terminated
+        NotificationManager.shared.center.removePendingNotificationRequests(withIdentifiers: [NotificationManager.shared.uuidString])
+        print("Done!")
+    }
+    
+}
